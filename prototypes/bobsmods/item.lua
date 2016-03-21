92702d0e903e3cf76a218c99d6deff22ea9d2a66
Afforess <afforess@gmail.com>
2016-03-20 21:22:46 -0400

    Check if the carbon and liquid fuel canister exist

--- prototypes/bobsmods/item.lua
+++ prototypes/bobsmods/item.lua
@@ -1,2 +1,6 @@
-data.raw.item["carbon"].fuel_value = "2.5MJ"
-data.raw.item["liquid-fuel-canister"].fuel_value = "50MJ"
+if data.raw.item["carbon"] then
+    data.raw.item["carbon"].fuel_value = "2.5MJ"
+end
+if data.raw.item["liquid-fuel-canister"] then
+    data.raw.item["liquid-fuel-canister"].fuel_value = "50MJ"
+end
