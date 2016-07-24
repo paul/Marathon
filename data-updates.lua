ec9bb0bdb1cbfd1ab95bad6dcf48a472c0f23e79
Afforess <afforess@gmail.com>
2016-07-24 12:08:12 -0400

    Add stdlib

--- data-updates.lua
+++ data-updates.lua
@@ -1,8 +1,23 @@
 
+log(serpent.dump(data.raw['recipe']))
 function marathon.update_recipe(name, values)
 	local recipe = data.raw.recipe[name]
 	if recipe then
 		for key, value in pairs(values) do
+			-- special ingredient error checking for debugging purposes
+			if key == 'ingredients' then
+				for _, item in pairs(value) do
+					if #item == 0 then
+						if not data.raw['item'][item.name] then
+							log("Unable to find item: " .. item.name .. ", failed to update recipe " .. name)
+						end
+					else
+						if not data.raw['item'][item[1]] then
+							log("Unable to find item: " .. item[1] .. ", failed to update recipe " .. name)
+						end
+					end
+				end
+			end
 			recipe[key] = value
 		end
 	end
@@ -16,8 +31,14 @@ function marathon.replace_recipe_item(recipe, prev_name, new_name)
 		for i = 1, #ingredients do
 			local ingredient_item = ingredients[i]
 			if ingredient_item[1] == prev_name then
+				if not data.raw['item'][ingredient_item[1]] then
+					log("Unable to find item: " .. ingredient_item[1] .. ", failed to update recipe " .. recipe)
+				end
 				ingredient_item[1] = new_name
 			elseif ingredient_item.name == prev_name then
+				if not data.raw['item'][ingredient_item.name] then
+					log("Unable to find item: " .. ingredient_item.name .. ", failed to update recipe " .. recipe)
+				end
 				ingredient_item.name = new_name
 			end
 		end
@@ -39,6 +60,9 @@ function marathon.add_new_recipe_item(recipe, item)
 			elseif ingredient_item.name == item_name then
 				return
 			end
+		end
+		if not data.raw['item'][item_name] then
+			log("Unable to find item: " .. item_name .. ", failed to update recipe " .. recipe)
 		end
 		table.insert(data.raw.recipe[recipe].ingredients, item)
 	end
@@ -67,6 +91,9 @@ function marathon.add_recipe_item(recipe, item)
 				ingredient_item.amount = ingredient_item.amount + item_amount
 				return
 			end
+		end
+		if not data.raw['item'][item_name] then
+			log("Unable to find item: " .. item_name .. ", failed to update recipe " .. recipe)
 		end
 		table.insert(data.raw.recipe[recipe].ingredients, item)
 	end
