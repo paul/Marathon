8589f221dafe1ed723e2fbb46f44b8acaef75d81
bowei liu <liubowei@gmail.com>
2017-02-24 23:17:08 -0800

    add error logs to indicate incompatibility with marathon mod.

---
+++ control.lua
@@ -0,0 +1,7 @@
+
+
+function onInit()
+  if marathomaton.incompatible == true then
+    game.print("Error detected! Marathomaton is incompatible with marathon mod. Changes have not been applied.") 
+  end 
+end
--- data-updates.lua
+++ data-updates.lua
@@ -1,17 +1,26 @@
 -- log(serpent.block(data.raw.recipe))
 
-require("prototypes.item")
-require("prototypes.recipe")
-require("prototypes.recipe-smelting")
-
-if bobmods and bobmods.config then
-	require("prototypes.bobsmods.recipe")
-	require("prototypes.bobsmods.recipe-circuit")
-	require("prototypes.bobsmods.recipe-misc")
-	require("prototypes.bobsmods.recipe-production")
-	require("prototypes.bobsmods.recipe-logistics")
-	require("prototypes.bobsmods.recipe-power")
-	require("prototypes.bobsmods.recipe-intermediate")
-	require("prototypes.bobsmods.recipe-smelting")
+if marathon ~= nil then
+  log('Error! Marathon mod detected -- cannot continue.')
+  marathomaton.incompatible = true
+else
+  marathomaton.incompatible = false
+  require("prototypes.item")
+  require("prototypes.recipe")
+  require("prototypes.recipe-smelting")
+  
+  if bobmods and bobmods.config then
+	  require("prototypes.bobsmods.recipe")
+	  require("prototypes.bobsmods.recipe-circuit")
+	  require("prototypes.bobsmods.recipe-misc")
+	  require("prototypes.bobsmods.recipe-production")
+	  require("prototypes.bobsmods.recipe-logistics")
+	  require("prototypes.bobsmods.recipe-power")
+	  require("prototypes.bobsmods.recipe-intermediate")
+	  require("prototypes.bobsmods.recipe-smelting")
+  end
 end
+
+
+
 
