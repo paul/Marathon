8848957e093b732d60ba0e5076293ec64ff11bf6
bowei liu <liubowei@gmail.com>
2017-02-25 00:34:27 -0800

    make iron axe craftable at start again

--- control.lua
+++ control.lua
@@ -1,9 +1,17 @@
 
+script.on_event(defines.events.on_player_created, function(event)
+  local player = game.players[event.player_index]
+  player.insert({name='iron-plate', count=1})
+end)
+
 --[[
 function detect_marathon()
   if marathomaton.incompatible == true then
     game.print("Error detected! Marathomaton is incompatible with marathon mod. Changes have not been applied.") 
   end 
 end
+
 script.on_configuration_changed(detect_marathon)
 --]]
+
+
